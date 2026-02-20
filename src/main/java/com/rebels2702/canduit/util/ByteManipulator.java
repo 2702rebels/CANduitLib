31e85b847e9baeaca776fd9dcbce89d8db94c1e1
turtlemaster890 <mastepieter16@gmail.com>
2026-02-20 17:48:25 -0500

    Merge remote-tracking branch 'origin/main' into test-code

--- src/main/java/com/rebels2702/canduit/util/ByteManipulator.java
+++ src/main/java/com/rebels2702/canduit/util/ByteManipulator.java
@@ -1,4 +1,5 @@
 package com.rebels2702.canduit.util;
+import java.util.ArrayList;
 
 /**
  * Utility class for manipulating bytes.
@@ -20,4 +21,65 @@ public class ByteManipulator {
         }
         return (int) out;
     }
+
+    /**
+     * Convert an array of ints and an array of bitsizes into a byte array, arranged in little-endian
+     * @param data The values to convert
+     * @param bitSizes The respective bitsizes of the values
+     * @param byteSize The byteSize of the array
+     */
+    public static byte[] packData(int[] data, int[] bitSizes, int byteSize){
+        byte[] packedData = new byte[byteSize];
+        if (data.length != bitSizes.length) return packedData;
+
+        long bitset = 0;
+        int totalSize = 0;
+        
+        // Adds all the bits to a 64 bit int
+        for (int idx = 0; idx<data.length;idx++){
+            bitset |= (
+                (long) (data[idx] & ((1L << bitSizes[idx])-1))
+                ) << totalSize;
+            totalSize += bitSizes[idx];        
+        }
+
+        if (totalSize >= 64) return packedData;
+
+        // Packs the bits into a byte array
+        long mask = 0xFF;
+        for (int idx = 0; idx < byteSize;idx++){
+            packedData[idx] = (byte) ((int) (bitset & mask));
+            packedData[idx] = (byte) ((int) (bitset & mask));
+            bitset >>= 8;
+        }
+        
+        return packedData;
+    }
+
+    /**
+     * Convert an array of bytes and an array of bitsizes into a int array, where each int i is a bitsize[i] slice of the array of bytes, which should be ordered in little endian
+     * @param data The values to convert
+     * @param bitSizes The respective bitsizes of the values
+     * @return an array of ints
+     */
+    public static int[] unpackData(byte[] data, int[] bitSizes){
+        int[] unpackedData = new int[bitSizes.length];
+        
+        // Merge little-endian data bytes into one long int
+        long dataInt = 0;
+        for (int idx = 0; idx<data.length; idx++){
+            dataInt |= (((long)data[idx]) << (idx*8));
+        }
+
+        // masks the first bitsizes[i] bits into the unpackeddata[i], then bitshifts right to move in the next set of bits to the 0 index
+        // Any bitSizes greater than the size of the data will be 0 and fail silently.
+        for (int idx = 0; idx<bitSizes.length; idx++){
+            unpackedData[idx] = (int) (dataInt & ((1L << bitSizes[idx])-1));
+            dataInt >>= bitSizes[idx];
+        }
+
+        return unpackedData;
+
+    }
+    
 }
